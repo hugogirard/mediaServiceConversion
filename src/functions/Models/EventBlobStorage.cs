885e8620aa54c35d1f7d2bad43c11114ea221b3f
hugogirard <hugo.girard@microsoft.com>
2022-09-12 15:19:05 -0400

    add missing folder

---
+++ src/functions/Models/EventBlobStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions.Models
+{
+    public class EventBlobStorage
+    {
+        public string Api { get; set; }
+
+        public string ClientRequestId { get; set; }
+
+        public string RequestId { get; set; }
+
+        public string Url { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string ContentLength { get; set; }
+    }
+}
