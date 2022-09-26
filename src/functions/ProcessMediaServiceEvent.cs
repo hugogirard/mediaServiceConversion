fdab968510914e5fbd5465fc286db28c30eeaef1
Hugo Girard <64166891+hugogirard@users.noreply.github.com>
2022-09-26 20:50:27 +0000

    added more logging for azure function

--- src/functions/ProcessMediaServiceEvent.cs
+++ src/functions/ProcessMediaServiceEvent.cs
@@ -29,45 +29,56 @@ namespace functions
         {
             // Get job ID (primary key of CosmosDB)
             var jobId = eventGridEvent.Subject.Split('/').Last();
+            
+            log.LogDebug($"Receive event: {eventGridEvent.EventType}");
 
             if (eventGridEvent.EventType == "Microsoft.Media.JobStateChange") 
             {
-                Uri collectionUri = UriFactory.CreateDocumentCollectionUri("videos", "mediaInsights");
-
-                // Retrieve the document and update it
-                IDocumentQuery<VideoEncodingJob> query = client.CreateDocumentQuery<VideoEncodingJob>(collectionUri)
-                                                               .Where(p => p.Id == jobId)
-                                                               .AsDocumentQuery();
-
-                // This should return only one document
-                while (query.HasMoreResults)
+                try
                 {
-                    foreach (VideoEncodingJob video in await query.ExecuteNextAsync())
-                    {
-                        // Updating the video status
-                        dynamic data = JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+                    Uri collectionUri = UriFactory.CreateDocumentCollectionUri("videos", "mediaInsights");
 
-                        if (!string.IsNullOrEmpty(data?.state))
+                    // Retrieve the document and update it
+                    IDocumentQuery<VideoEncodingJob> query = client.CreateDocumentQuery<VideoEncodingJob>(collectionUri)
+                                                                .Where(p => p.Id == jobId)
+                                                                .AsDocumentQuery();
+
+                    // This should return only one document
+                    while (query.HasMoreResults)
+                    {
+                        foreach (VideoEncodingJob video in await query.ExecuteNextAsync())
                         {
-                            if (Enum.TryParse(data.state, out JobState newState))
+                            // Updating the video status
+                            dynamic data = JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+
+                            if (!string.IsNullOrEmpty(data?.state))
                             {
-                                video.State = newState;
+                                if (Enum.TryParse(data.state, out JobState newState))
+                                {
+                                    video.State = newState;
+                                }
+                                else 
+                                {
+                                    video.Error = $"Cannot parse state in event: {data}";
+                                }
                             }
                             else 
                             {
-                                video.Error = $"Cannot parse state in event: {data}";
+                                video.Error = $"Not state in event: {data}";
                             }
-                        }
-                        else 
-                        {
-                            video.Error = $"Not state in event: {data}";
-                        }
 
-                        await videos.AddAsync(video);
+                            await videos.AddAsync(video);
 
-                        break;
-                    }
+                            break;
+                        }
+                    }                                                                         
                 }
+                catch (System.Exception ex)
+                {
+                    log.LogError(ex.Message,ex);
+                    log.LogError(ex.StackTrace);
+                }
+
             }
             
         }
